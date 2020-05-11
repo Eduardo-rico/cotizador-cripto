808a393147a55a44f00baf9199c651d40b5c8f43
Eduardo Rico <emrs94@gmail.com>
2020-05-11 14:51:12 -0500

    Add files via upload

---
+++ build/precache-manifest.0d60fe91ca22d7c176382010083ad16c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcca304023a553d78e478b6cdf3575ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4c063502d533dcacbfe",
+    "url": "/static/css/main.ff3c0b12.chunk.css"
+  },
+  {
+    "revision": "176dd67df79453478f65",
+    "url": "/static/js/2.0849ebb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0849ebb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4c063502d533dcacbfe",
+    "url": "/static/js/main.208159bf.chunk.js"
+  },
+  {
+    "revision": "585071355f2bbc587a2c",
+    "url": "/static/js/runtime-main.52d75eaf.js"
+  },
+  {
+    "revision": "96e45d694833f161c9dc02ab9dde4a6b",
+    "url": "/static/media/cryptomonedas.96e45d69.png"
+  }
+]);
